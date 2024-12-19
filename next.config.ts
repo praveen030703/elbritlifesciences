6a2cc6ef495916ddbe0b854aebeebe8589350aa4
praveen030703 <praveenrs1503@gmail.com>
2024-12-19 16:24:38 +0530

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/elbritlifescience-praveen",
+  //basePath: "/elbritlifescience-praveen",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
